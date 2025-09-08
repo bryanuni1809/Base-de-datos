099dc980e7d0bf4bacd2cad8ef827d9fcc062f43
bryanuni1809 <bryanuni1809@gmail.com>
2025-09-07 19:01:32 -0500

    Operadores Logicos v2

--- Semana 4/SQLQuery4.sql
+++ Semana 4/SQLQuery4.sql
@@ -6,3 +6,15 @@ WHERE NOT ListPrice =0;
 SELECT ProductID,Name,ListPrice
 FROM Production.Product
 WHERE ListPrice BETWEEN 500 AND 1000;
+--IN
+SELECT BusinessEntityID,FirstName,LastName
+FROM Person.Person
+WHERE PersonType IN ('EM','IN','SP');
+--LIKE
+SELECT BusinessEntityID,FirstName,LastName
+FROM Person.Person
+WHERE LastName LIKE 'A%';
+--NULL
+SELECT BusinessEntityID, MiddleName, FirstName, LastName
+FROM Person.Person
+WHERE MiddleName IS NULL;
